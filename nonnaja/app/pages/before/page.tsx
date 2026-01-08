aea214d1fdfbf0d8c1bcbd7346eeac4359e9a237
Funnyy153 <funnyy153@gmail.com>
2026-01-08 13:09:00 +0700

    ฟ

---
+++ nonnaja/app/pages/before/page.tsx
@@ -0,0 +1,17 @@
+import SheetData from "../../components/SheetData";
+
+export default function BeforePage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
+      <main className="flex min-h-screen w-full max-w-7xl flex-col items-center py-8 px-4 bg-white ">
+        <div className="w-full">
+          <h1 className="text-3xl font-bold mb-6 text-center text-black dark:text-zinc-50">
+            ข้อมูลจาก Google Sheets
+          </h1>
+          <SheetData />
+        </div>
+      </main>
+    </div>
+  );
+}
+
